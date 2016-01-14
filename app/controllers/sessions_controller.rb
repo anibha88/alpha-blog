58b68e16f0414ead1dc02a7fec0fdc01591c2d25
anibha88 <anibha88@gmail.com>
2016-01-14 07:07:27 +0530

    create and destroy methods for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # render 'new'
-    debugger
+    # 1. Find the user based on the email
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # You are basically saving your user id in the session's hash which is 
+      # backed by your browser's cookies
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your credentials"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 
 end
